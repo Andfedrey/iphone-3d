12bf05ebe11943a441b02a49f1ce8c4ceccfa551
Andrey Fedorov <craewell@gmail.com>
2023-04-16 18:14:23 +0300

    animation 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Jumbotron } from "./components/Jumbotron";
+import { Nav } from "./components/Nav";
+import { SoundSection } from './components/SoundSection'
+import { DisplaySection } from './components/DisplaySection'
+import { WebgiViewer } from "./components/WebgiViewer";
 
 function App() {
-
   return (
     <div className="App">
-      Hello
+      <Nav></Nav>
+      <Jumbotron></Jumbotron>
+      <SoundSection></SoundSection>
+      <DisplaySection></DisplaySection>
+      <WebgiViewer></WebgiViewer>
     </div>
   );
 }
